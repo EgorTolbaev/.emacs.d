8b0246eb5beb45e08d5af3ab673d2f3f739c20c1
EgorTolbaev <egor05.09.97@gmail.com>
2021-05-29 11:39:41 +0500

    add Org bullets

--- custom.el
+++ custom.el
@@ -4,9 +4,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("93ed23c504b202cf96ee591138b0012c295338f38046a1f3c14522d4a64d7308" "990e24b406787568c592db2b853aa65ecc2dcd08146c0d22293259d400174e37" default))
+   '("277886b910ce4280438d9ce4ebde4f9823c9452400acfe6dc9f01248dfa05aa4" "93ed23c504b202cf96ee591138b0012c295338f38046a1f3c14522d4a64d7308" "990e24b406787568c592db2b853aa65ecc2dcd08146c0d22293259d400174e37" default))
  '(package-selected-packages
-   '(dashboard all-the-icons-dired doom-modeline doom-themes projectile company-box company reverse-im sr-speedbar magit treemacs use-package)))
+   '(org-bullets dashboard all-the-icons-dired doom-modeline doom-themes projectile company-box company reverse-im sr-speedbar magit treemacs use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
