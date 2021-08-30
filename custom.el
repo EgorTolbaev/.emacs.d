c5715d32fb2f3633a17e217bd059381e86bce243
EgorTolbaev <egor05.09.97@gmail.com>
2021-08-30 14:40:04 +0500

    Add Markdown

--- custom.el
+++ custom.el
@@ -4,7 +4,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("0be5c1a44e0a877aa9cedae800c438d09efe12bbc7cbc7f787d43c5b8e7eb0db" "277886b910ce4280438d9ce4ebde4f9823c9452400acfe6dc9f01248dfa05aa4" default)))
+   '("0be5c1a44e0a877aa9cedae800c438d09efe12bbc7cbc7f787d43c5b8e7eb0db" "277886b910ce4280438d9ce4ebde4f9823c9452400acfe6dc9f01248dfa05aa4" default))
+ '(package-selected-packages
+   '(markdown-mode which-key web-mode use-package treemacs sr-speedbar reverse-im pyvenv projectile org-bullets magit ivy-rich helpful flycheck doom-themes doom-modeline dashboard counsel company-jedi company-box anaconda-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
