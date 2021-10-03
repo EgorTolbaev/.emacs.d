72caf09eed707fb5e502cae81c170605a35bd304
EgorTolbaev <egor05.09.97@gmail.com>
2021-10-03 17:57:09 +0500

    customization org-mode

--- custom.el
+++ custom.el
@@ -6,7 +6,7 @@
  '(custom-safe-themes
    '("a7b20039f50e839626f8d6aa96df62afebb56a5bbd1192f557cb2efb5fcfb662" "a9a67b318b7417adbedaab02f05fa679973e9718d9d26075c6235b1f0db703c8" "0be5c1a44e0a877aa9cedae800c438d09efe12bbc7cbc7f787d43c5b8e7eb0db" "277886b910ce4280438d9ce4ebde4f9823c9452400acfe6dc9f01248dfa05aa4" default))
  '(package-selected-packages
-   '(dired-single treemacs-icons-dired engine-mode toc-org markdown-mode which-key web-mode use-package treemacs sr-speedbar reverse-im pyvenv projectile org-bullets magit ivy-rich helpful flycheck doom-themes doom-modeline dashboard counsel company-jedi company-box anaconda-mode)))
+   '(visual-fill-column org dired-single treemacs-icons-dired engine-mode toc-org markdown-mode which-key web-mode use-package treemacs sr-speedbar reverse-im pyvenv projectile org-bullets magit ivy-rich helpful flycheck doom-themes doom-modeline dashboard counsel company-jedi company-box anaconda-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
