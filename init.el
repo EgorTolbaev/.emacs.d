09af227d797741e10a233a506ea4534d246f399f
egortolbaev <egor05.09.97@gmail.com>
2022-02-06 17:41:10 +0500

    setting dired-single

--- init.el
+++ init.el
@@ -253,7 +253,12 @@
   :ensure nil
   :commands (dired dired-jump)
   :bind (("C-x C-j" . dired-jump))
-  :custom ((dired-listing-switches "-agho --group-directories-first")))
+  :custom ((dired-listing-switches "-agho --group-directories-first"))
+  :config
+  (define-key dired-mode-map "h"
+    'dired-single-up-directory)
+  (define-key dired-mode-map "l"
+    'dired-single-buffer))
 
 (use-package dired-single)
 
